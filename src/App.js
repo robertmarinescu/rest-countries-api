cdb0250a7d44d3ba971759c31cd376193ac8865b
Robert Marinescu <robert.marinescu93@gmail.com>
2022-06-21 17:40:42 +0300

    Fetch data from the server and display the countries info correctly

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React, {useState} from "react";
-import Header from "./components/Header";
-import SearchIcon from '@mui/icons-material/Search';
-import Country from "./components/Country";
+import React, {useState, useEffect} from "react"
+import Header from "./components/Header"
+import SearchIcon from '@mui/icons-material/Search'
+import Country from "./components/Country"
 import { Routes, Route} from 'react-router-dom'
-import CountryDetails from "./components/CountryDetails";
+import CountryDetails from "./components/CountryDetails"
+import axios from "axios"
+
+const URL = 'https://restcountries.com/v2/all'
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false)
+  const [countries, setCountries] = useState([])
 
   const switchMode = () => {
     setDarkMode(prevState => !prevState)
   }
+
+  const fetchCountriesData = async () => {
+    const response = await fetch(URL);
+    const data = await response.json()
+
+    setCountries(data)
+  }
+
+  useEffect(() => {
+    try{
+      fetchCountriesData()
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
 
   return (
     <div className={`app ${darkMode ? 'darkMode' : ''}`}>
@@ -37,7 +56,22 @@ function App() {
           </div>
 
           <div className="countries">
-            <Country darkMode={darkMode}/>
+            {/* <Country darkMode={darkMode}/> */}
+            {
+              countries.map(country => {
+                const {numericCode, name, population, region, capital, flag} = country
+                return <Country 
+                  key={name} 
+                  code = {numericCode}
+                  darkMode={darkMode}
+                  name={name}
+                  population={population}
+                  region={region}
+                  capital={capital}
+                  flag={flag}
+                />
+              }
+            )}
           </div>
         </div>
         }/>
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,21 +1,21 @@
 import React from 'react'
 
-function Country({darkMode}) {
+function Country({darkMode, name, population, region, capital, flag}) {
   return (
-    <div className={`country ${darkMode ? 'darkMode' : ''}]`}>
+    <div className={`country ${darkMode ? 'darkMode' : ''}`}>
         <div className='flag_container'>
-            <img src='https://flagcdn.com/be.svg' alt=''></img>
+            <img src={flag} alt=''></img>
         </div>
         <div className='details'>
-            <h3 className='name'>Name</h3>
+            <h3 className='name'>{name}</h3>
             <p>Population: {""}
-                <span className={`values ${darkMode ? 'darkMode' : ''}]`}>Test</span>
+                <span className={`values ${darkMode ? 'darkMode' : ''}`}>{population}</span>
             </p>
             <p>Region: {""}
-                <span className={`values ${darkMode ? 'darkMode' : ''}]`}>Test</span>
+                <span className={`values ${darkMode ? 'darkMode' : ''}`}>{region}</span>
             </p>
             <p>Capital: {""}
-                <span className={`values ${darkMode ? 'darkMode' : ''}]`}>Test</span>
+                <span className={`values ${darkMode ? 'darkMode' : ''}`}>{capital}</span>
             </p>
         </div>
     </div>
